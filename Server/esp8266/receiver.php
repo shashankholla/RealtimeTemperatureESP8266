a2e96cbd3b96926bc5e5086873b59b7c370da9a0
unknown <shashu1999@gmail.com>
2018-09-27 23:45:32 +0530

    All updated to 27Sept2018

--- Server/esp8266/receiver.php
+++ Server/esp8266/receiver.php
@@ -1,6 +1,6 @@
 <?php
-$var1 = $_GET{'apples'};
-$var2 = $_GET{'oranges'};
+$var1 = $_GET{'temp'};
+$var2 = $_GET{'humidity'};
 
 $fileContent =$var1." ".$var2."\n";
 file_put_contents('myFile.txt',$fileContent,FILE_APPEND);
