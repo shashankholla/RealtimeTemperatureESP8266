d246220b3fc15ac56e8bee2de73938b3f3437f0f
shashankholla <shashu1999@gmail.com>
2020-07-24 11:30:37 +0530

    Update readme

--- Arduino/esptest/esptest.ino
+++ Arduino/esptest/esptest.ino
@@ -1,125 +1,102 @@
 #include <SoftwareSerial.h>
-#include<dht.h>
-SoftwareSerial esp8266(2,3); 
-
-
-
+#include <dht.h>
+SoftwareSerial esp8266(10, 11);
 
 #define DEBUG true
 
-int a = 1;
 dht DHT;
+int t, h;
+String ipad = "my-smart-farm.herokuapp.com";
+// String ipad = "192.168.1.6";
 
-
-float t,h;
-String ipad = "192.168.43.23";
 void setup()
 
-  
-  
-  {
-    Serial.println("Here goes");
-    
-    
-    Serial.begin(9600);
-    esp8266.begin(9600); 
-    esp8266Serial("AT+RST\r\n", 5000, DEBUG); 
-    esp8266Serial("AT+CWMODE_CUR=1\r\n", 5000, DEBUG); 
-   
-   
-   
-   // esp8266Serial("AT+UART_DEF=9600,8,1,0,0\r\n",5000,DEBUG);
-   
-    
-    esp8266Serial("AT+CWJAP=\"ASUS_X00TD\",\"testtest123\"\r\n", 5000, DEBUG);
-    delay(1000);
-    
-    
-    String x = "AT+PING=\""+ipad+"\"\r\n";
-    
-    
-    esp8266Serial(x,5000,DEBUG);
-    delay(1000);                     
-  
-   // esp8266Serial("AT+CIFSR\r\n", 5000, DEBUG)
-    //esp8266Serial("AT+CIPMUX=1\r\n", 5000, DEBUG);
-    //esp8266Serial("AT+CIPSERVER=1,80\r\n", 5000, DEBUG);
-  }
-
-void loop()
-  {
-    
-
-    int readData = DHT.read11(7);
-    t = DHT.temperature;
-    h = DHT.humidity;
-    
-    if(DEBUG)
-    {Serial.print("TEMPERATURE");
-    Serial.println(t);
-    }
-    
-    sendData(1,1);
-   
-
-
-      
-  }
-
-void sendData(int a,int b)
 {
-  Serial.println("Sending");
-  String TCP = "AT+CIPSTART=\"TCP\",\"" + ipad + "\",80\r\n";
-   esp8266Serial(TCP,5000,DEBUG);    
-   delay(1000);
- 
-  int ti = (int) t;
-  int hi = (int) h;
+  pinMode(13, OUTPUT);
+  pinMode(A0, INPUT);
+  pinMode(12, OUTPUT);
+  Serial.begin(9600);
+  esp8266.begin(9600);
 
- 
-  String s = "GET /esp8266/receiver.php?temp="+(String) ti+ "&humidity="+ (String)hi +" HTTP/1.1\r\nHost: "+ipad+ "\r\n\r\n" ;
-  
-      String lengt = "AT+CIPSEND=" + (String) s.length() + "\r\n";
-     // Serial.print(lengt);
-    //  Serial.println(s.length());
+  esp8266Serial("AT+RST\r\n", 1200, DEBUG);
+  esp8266Serial("AT+CWMODE=1\r\n", 1200, DEBUG);
 
-      
-     esp8266Serial(lengt,1000,DEBUG);
-     
-      
-  
-      esp8266Serial(s, 5000, DEBUG);
-    
-    //Serial.print(t);
-    delay(1000);
-   // Serial.println("Sent data");
+  // esp8266Serial("AT+UART_DEF=9600,8,1,0,0\r\n",5000,DEBUG);
 
+  // esp8266Serial("AT+CWJAP=\"Shashank\",\"mathura2\"\r\n", 2500, DEBUG);
+  esp8266Serial("AT+CWJAP=\"Desktop\",\"11111111\"\r\n", 2500, DEBUG);
+  delay(500);
 
-  
+  String x = "AT+PING=\"" + ipad + "\"\r\n";
+
+  esp8266Serial(x, 2000, DEBUG);
+  delay(250);
 }
 
+void loop()
+{
+
+  int readData = DHT.read11(7);
+  t = DHT.temperature;
+  h = DHT.humidity;
+  Serial.print("AnalogVolt" + (String)analogRead(A0));
+  int x = map(analogRead(A0), 300, 1024, 100, 0);
+  sendData(t, h, x);
+}
+
+void sendData(int t, int h, int moisture)
+{
+
+  String TCP = "AT+CIPSTART=\"TCP\",\"" + ipad + "\",80\r\n";
+  esp8266Serial(TCP, 2000, DEBUG);
+  delay(250);
+
+  int wL = 0;
+  int sl = 0;
+
+  String s = "GET /receiver.php?temp=" + (String)t +
+             "&humidity=" + (String)h +
+             "&waterLevel=" + (String)wL +
+             "&sunlight=" + (String)sl +
+             "&moisture=" + (String)moisture +
+             " HTTP/1.1\r\nHost: " + ipad + "\r\n\r\n";
+
+  String length = "AT+CIPSEND=" + (String)s.length() + "\r\n";
+  esp8266Serial(length, 500, DEBUG);
+  Serial.print("Sending:");
+  Serial.println(s);
+  String resp = esp8266Serial(s, 1500, DEBUG);
+  char mStatus = resp[resp.indexOf("motor") + 6];
+  if (mStatus == '0')
+  {
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
+  }
+  else
+  {
+    digitalWrite(12, HIGH);
+    digitalWrite(13, HIGH);
+  }
+}
 
 String esp8266Serial(String command, const int timeout, boolean debug)
-  {
-    String response = "";
-    esp8266.print(command);
-    long int time = millis();
-    while ( (time + timeout) > millis())
-      {
-        while (esp8266.available())
-          {
-            char c = esp8266.read();
-            response += c;
-          }
-      }
-    if (debug)
-        {
-        Serial.print(response);
-      }
+{
+  String response = "";
+  esp8266.print(command);
 
-    if(response.indexOf("SUCCESS") > 0)
+  long int time = millis();
+  while ((time + timeout) > millis())
+  {
+    while (esp8266.available())
     {
-      Serial.println("\n Yolo sent NOW!!!!!!!!!!! ");
+      char c = esp8266.read();
+      response += c;
     }
-    return response; 
+  }
+  if (DEBUG)
+  {
+    Serial.print(response);
+  }
+
+  return response;
 }
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # RealtimeTemperatureESP8266
-Send realtime temperature and humidity data from Arduino UNO using ESP8266 to a PC based XAMPP Server.
+
+Send realtime temperature and humidity data from Arduino UNO using ESP8266 to website hosted on your favorite dynamic site hosting platform.
+
+    
+
+- This project aims at collecting real-time physical data from an agricultural farm, displaying it in a graphical way and automating farming process.
+- It involves collecting physical parameters like soil moisture, water level in water tanks, temperature, humidity and sunlight.
+- It aims at making this data available on any device connected to the Internet by storing it on a custom local server.
+- It also involves developing an Android based application to receive notifications upon critical conditions in farm.
+- It aims at implementing a web and app based system to control irrigation.
+
+Screen grab of the site hosted (currently not functional because the hardware is not connected)
+
+![](C:\Users\shash\AppData\Roaming\marktext\images\2020-07-24-11-20-38-image.png)
+
+
+
+Log data and observe change over a period of time
+
+    ![](C:\Users\shash\AppData\Roaming\marktext\images\2020-07-24-11-20-17-image.png)
+
+
+
+Get web notifications if temperature crosses set value
+
+![](C:\Users\shash\AppData\Roaming\marktext\images\2020-07-24-11-25-08-image.png)
+
+
+
+Arduino wiring
+
+![](C:\Users\shash\AppData\Roaming\marktext\images\2020-07-24-11-11-52-image.png)
+
+
+
+1. 
+
+
+
+The communication flow is briefly explained through this diagram
+
+![](C:\Users\shash\AppData\Roaming\marktext\images\2020-07-24-11-21-26-image.png)
+
+![](C:\Users\shash\AppData\Roaming\marktext\images\2020-07-24-11-25-33-image.png)
